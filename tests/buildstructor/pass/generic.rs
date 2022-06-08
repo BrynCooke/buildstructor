c9d3b6f63e8cb3a2ec110f9f5352ed52473b48df
Bryn Cooke <bryncooke@gmail.com>
2022-06-08 18:07:17 +0200

    Add a few more tests for generics.

--- tests/buildstructor/pass/generic.rs
+++ tests/buildstructor/pass/generic.rs
@@ -9,8 +9,26 @@ impl<T> Foo<T> {
     fn new(simple: T) -> Foo<T> {
         Self { simple }
     }
+
+    #[builder]
+    fn bound2_new(simple: T) -> Foo<T>
+    where
+        T: std::fmt::Debug,
+    {
+        Self { simple }
+    }
+}
+
+#[buildstructor]
+impl<T: std::fmt::Debug> Foo<T> {
+    #[builder]
+    fn bound1_new(simple: T) -> Foo<T> {
+        Self { simple }
+    }
 }
 
 fn main() {
     let _ = Foo::builder().simple(3).build();
+    let _ = Foo::bound1_builder().simple(3).build();
+    let _ = Foo::bound2_builder().simple(3).build();
 }
